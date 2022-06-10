35c6072d4ca39f1848bd45d94f9ab4b622cd63bd
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2022-06-10 19:21:18 +0300

    Add app.ts

---
+++ server/app.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response} from "express";
+
+const app: Express = express()
+
+const port = process.env.PORT || 4000;
+
+app.get('/', (req: Request, res: Response) =>  {
+    res.send('Qsha!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
