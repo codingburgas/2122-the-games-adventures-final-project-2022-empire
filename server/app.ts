efa8d79e8abb8be9635d178344da8ecf939fd8d6
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2022-06-10 23:39:59 +0300

    Merge the dockerfiles for the server and client. - Add mysql to the dockerfile and basic connection for testing in the dockerfile

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,25 @@
 import express, { Express, Request, Response} from "express";
+import mysql, {Connection} from 'mysql2';
 
 const app: Express = express()
 
 const port = process.env.PORT || 4000;
+
+const connection: Connection = mysql.createConnection({
+    host: 'mysqldb',
+    user: 'root',
+    password: 'idk',
+    port: 3306,
+})
+
+connection.connect((err) => {
+    if(err)
+    {
+        console.log(err);
+        return;
+    }
+    console.log("Pog");
+})
 
 app.get('/', (req: Request, res: Response) =>  {
     res.send('Qsha!');
