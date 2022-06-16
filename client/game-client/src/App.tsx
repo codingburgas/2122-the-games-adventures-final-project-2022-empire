f7e3a8de3877f9a58de33c3f2c4e61b3f9705b80
AEManov20 <AEManov20@codingburgas.bg>
2022-06-16 20:29:55 +0300

    Implement engine/emscripten types in TS

--- client/game-client/src/App.tsx
+++ client/game-client/src/App.tsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
+import { useContext, useState, useMemo } from 'react';
+import { EmscriptenModule } from './EmscriptenTypes';
+import { EngineContext, FunctionBindings, CEntity } from './EngineContext';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const engine = useContext(EngineContext);
+  const [audio, setAudio] = useState<CEntity | null>(null);
+
+  if (!engine) return <></>;
+
+  // componentDidMount
+  useMemo(() => {
+    setAudio(new engine.CEntity());
+    console.log('initialized entity!', audio);
+    
+    // cleanup
+    return () => {
+      console.log('uninitialized entity!', audio);
+      if (audio) engine.destroy(audio);
+    }
+  }, []);
 
   return (
     <>
       <h1>le nice app</h1>
       <h2>le nice count: {count}</h2>
       <button onClick={() => setCount(count + 1)}>le nice button</button>
+      <h2>le nice audio: {audio?.ID}</h2>
     </>
   )
 }
 
-export default App
+export default App;
--- client/game-client/src/main.tsx
+++ client/game-client/src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import createModule from './engine.js';
+import { EngineContext, EngineModule } from './EngineContext';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+createModule().then((module: EngineModule | null) => {
+  if (module) console.log('wasm initialized!');
+  else console.log('wasm could not be initialized!');
+
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <EngineContext.Provider value={module}>
+      <App />
+    </EngineContext.Provider>
+  )
+});
+
