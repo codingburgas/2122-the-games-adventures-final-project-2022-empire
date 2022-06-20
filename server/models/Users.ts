42fc729090376f619aeefc2b18e3d606a1ff0113
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2022-06-20 19:41:27 +0300

    Fix merge conflict again

--- server/models/Users.ts
+++ server/models/Users.ts
@@ -20,12 +20,16 @@ class Users extends BaseModel {
       .input("Username", sql.VarChar, data.username)
       .input("Password", sql.VarChar, await bcrypt.hash(data.password, salt))
       .input("Salt", sql.VarChar, salt)
-      .query("INSERT INTO Users(Username, Password, Salt) VALUES(@Username, @Password, @Salt); SELECT SCOPE_IDENTITY() AS id")
+      .query(
+        "INSERT INTO Users(Username, Password, Salt) VALUES(@Username, @Password, @Salt); SELECT SCOPE_IDENTITY() AS id"
+      )
       .then((result) => {
         return new UserReturnData(result.recordset[0].id, data.username);
       })
       .catch((_) => {
-        this.loggerManager.logWarn(`Register failed. Reason: Database refuse ` + _);
+        this.loggerManager.logWarn(
+          `Register failed. Reason: Database refuse ` + _
+        );
 
         return null;
       });
@@ -37,7 +41,9 @@ class Users extends BaseModel {
       .input("LoginUsername", sql.VarChar, data.username)
       .query("Select Salt as salt FROM Users WHERE Username = @LoginUsername")
       .catch((err) => {
-        this.loggerManager.logWarn(`Login failed. Reason: Database refuse ` + err);
+        this.loggerManager.logWarn(
+          `Login failed. Reason: Database refuse ` + err
+        );
 
         return null;
       });
@@ -47,52 +53,83 @@ class Users extends BaseModel {
     return this.connection
       .request()
       .input("LoginUsername", sql.VarChar, data.username)
-      .input("LoginPassword", sql.VarChar, await bcrypt.hash(data.password, salt.recordsets[0][0].salt))
-      .query("SELECT Id AS id FROM Users WHERE Username = @LoginUsername AND Password = @LoginPassword")
+      .input(
+        "LoginPassword",
+        sql.VarChar,
+        await bcrypt.hash(data.password, salt.recordsets[0][0].salt)
+      )
+      .query(
+        "SELECT Id AS id FROM Users WHERE Username = @LoginUsername AND Password = @LoginPassword"
+      )
       .then((result) => {
         if (result.recordset[0])
           return new UserReturnData(result.recordset[0].id, data.username);
 
-        this.loggerManager.logWarn(`Login failed. Reason: Invalid username or password.`);
-
+        this.loggerManager.logWarn(
+          `Login failed. Reason: Invalid username or password.`
+        );
         return null;
       })
       .catch((err) => {
-        this.loggerManager.logWarn(`Login failed. Reason: Database refuse ` + err);
+        this.loggerManager.logWarn(
+          `Login failed. Reason: Database refuse ` + err
+        );
         return null;
       });
-  };
+  };и аз
 
   getUsers = (): this => {
     this._transitionalVar = this.connection
-    .request()
-    .query("SELECT Id as id, Username as username FROM Users");
+      .request()
+      .query("SELECT Id as id, Username as username FROM Users");
 
-     return this;
+    return this;
   };
 
   by = async (data: FilterData): Promise<UserReturnData[]> => {
-    const keys: Array<keyof FilterData> = ["id", "username"];
+      const keys: Array<keyof FilterData> = ["id", "username"];
 
-    let filterData: UserReturnData[] = [];
+      let filterData: UserReturnData[] = [];
 
-    return this._transitionalVar.then((result) => {
-        for (let key of keys) {
-          if (data[key]) {
-            for (let object of result.recordset) {
-              if (object[key] == data[key]) {
-                filterData.push(new UserReturnData(object.id, object.username));
+      return this._transitionalVar
+          .then((result) => {
+              for (let key of keys) {
+                  if (data[key]) {
+                      for (let object of result.recordset) {
+                          if (object[key] == data[key]) {
+                              filterData.push(new UserReturnData(object.id, object.username));
+                          }
+                      }
+                  }
               }
-            }
-          }
-        }
-        return filterData;
-      })
-      .catch((err) => {
-        this.loggerManager.logWarn(`Get users failed. Reason: Database refuse ` + err);
-        return [];
-      });
-  };
+              return filterData;
+          })
+          .catch((err) => {
+              this.loggerManager.logWarn(
+                  `Get users failed. Reason: Database refuse ` + err
+              );
+              return [];
+          });
+    ;}
+
+      deleteUserById = (id: number): void => {
+          this.connection.request()
+              .input('DeleteId', sql.Int, id)
+              .query('DELETE FROM Users WHERE Id = @DeleteId')
+              .catch((err) => {
+                  console.log(err);
+              });
+      };
+
+      updateUserById = (data: {oldUsername: string, newUsername: string}): Promise<boolean> => {
+          return this.connection.request()
+              .input('OldUsername', sql.VarChar, data.oldUsername)
+              .input('NewUsername', sql.VarChar, data.newUsername)
+              .query('UPDATE Users SET Username = @NewUsername WHERE Username = @OldUsername')
+              .then((result) => {
+                  return !!result.rowsAffected[0];
+              })
+      };
 }
 
 const User: Users = new Users();
