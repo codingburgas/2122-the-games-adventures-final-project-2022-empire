3a7343ea22fa7c6d6b291bd4a9ac28d1f35add2c
Stoyan Ivanov <ssivanov19@codingburgas.bg>
2022-06-20 21:54:56 +0300

    Add initial react pages

--- client/game-client/src/App.tsx
+++ client/game-client/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Index = lazy(() => import("./pages/index"));
 const Game = lazy(() => import("./pages/game"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
 //const UserContext = createContext();
 
 function App() {  
@@ -14,6 +16,12 @@ function App() {
           </Routes>
           <Routes>
             <Route path="/game" element={<Game />} />
+          </Routes>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Routes>
+            <Route path="/register" element={<Register />} />
           </Routes>
         </Suspense>
     </Router>
--- client/game-client/src/pages/index.tsx
+++ client/game-client/src/pages/index.tsx
@@ -7,6 +7,18 @@ function Index() {
       <Link to="/game">
         <button>Go to game</button>
       </Link>
+      <br/><br/>
+      <Link to="/login">
+        <button>Go to login</button>
+      </Link>
+      <br/><br/>
+      <Link to="/register">
+        <button>Go to register</button>
+      </Link>
+      <br/><br/>
+      <Link to="/account">
+        <button>Go to account</button>
+      </Link>
     </>
   );
 }
