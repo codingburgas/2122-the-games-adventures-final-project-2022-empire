96707a18643bedf1385c8a48f79a6f50dc4b4d21
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2022-06-20 19:29:29 +0300

    Reformat code

--- server/models/Users.ts
+++ server/models/Users.ts
@@ -20,16 +20,12 @@ class Users extends BaseModel {
       .input("Username", sql.VarChar, data.username)
       .input("Password", sql.VarChar, await bcrypt.hash(data.password, salt))
       .input("Salt", sql.VarChar, salt)
-      .query(
-        "INSERT INTO Users(Username, Password, Salt) VALUES(@Username, @Password, @Salt); SELECT SCOPE_IDENTITY() AS id"
-      )
+      .query("INSERT INTO Users(Username, Password, Salt) VALUES(@Username, @Password, @Salt); SELECT SCOPE_IDENTITY() AS id")
       .then((result) => {
         return new UserReturnData(result.recordset[0].id, data.username);
       })
       .catch((_) => {
-        this.loggerManager.logWarn(
-          `Register failed. Reason: Database refuse ` + _
-        );
+        this.loggerManager.logWarn(`Register failed. Reason: Database refuse ` + _);
 
         return null;
       });
@@ -41,9 +37,7 @@ class Users extends BaseModel {
       .input("LoginUsername", sql.VarChar, data.username)
       .query("Select Salt as salt FROM Users WHERE Username = @LoginUsername")
       .catch((err) => {
-        this.loggerManager.logWarn(
-          `Login failed. Reason: Database refuse ` + err
-        );
+        this.loggerManager.logWarn(`Login failed. Reason: Database refuse ` + err);
 
         return null;
       });
@@ -53,37 +47,28 @@ class Users extends BaseModel {
     return this.connection
       .request()
       .input("LoginUsername", sql.VarChar, data.username)
-      .input(
-        "LoginPassword",
-        sql.VarChar,
-        await bcrypt.hash(data.password, salt.recordsets[0][0].salt)
-      )
-      .query(
-        "SELECT Id AS id FROM Users WHERE Username = @LoginUsername AND Password = @LoginPassword"
-      )
+      .input("LoginPassword", sql.VarChar, await bcrypt.hash(data.password, salt.recordsets[0][0].salt))
+      .query("SELECT Id AS id FROM Users WHERE Username = @LoginUsername AND Password = @LoginPassword")
       .then((result) => {
         if (result.recordset[0])
           return new UserReturnData(result.recordset[0].id, data.username);
 
-        this.loggerManager.logWarn(
-          `Login failed. Reason: Invalid username or password.`
-        );
+        this.loggerManager.logWarn(`Login failed. Reason: Invalid username or password.`);
+
         return null;
       })
       .catch((err) => {
-        this.loggerManager.logWarn(
-          `Login failed. Reason: Database refuse ` + err
-        );
+        this.loggerManager.logWarn(`Login failed. Reason: Database refuse ` + err);
         return null;
       });
   };
 
   getUsers = (): this => {
     this._transitionalVar = this.connection
-      .request()
-      .query("SELECT Id as id, Username as username FROM Users");
+    .request()
+    .query("SELECT Id as id, Username as username FROM Users");
 
-    return this;
+     return this;
   };
 
   by = async (data: FilterData): Promise<UserReturnData[]> => {
@@ -91,8 +76,7 @@ class Users extends BaseModel {
 
     let filterData: UserReturnData[] = [];
 
-    return this._transitionalVar
-      .then((result) => {
+    return this._transitionalVar.then((result) => {
         for (let key of keys) {
           if (data[key]) {
             for (let object of result.recordset) {
@@ -105,9 +89,7 @@ class Users extends BaseModel {
         return filterData;
       })
       .catch((err) => {
-        this.loggerManager.logWarn(
-          `Get users failed. Reason: Database refuse ` + err
-        );
+        this.loggerManager.logWarn(`Get users failed. Reason: Database refuse ` + err);
         return [];
       });
   };
