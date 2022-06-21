8bbfda3d5c6f7247e8dd66dd91bc17dac1f4cda4
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2022-06-21 14:49:17 +0300

    Add login and register function to client app

--- client/game-client/src/App.tsx
+++ client/game-client/src/App.tsx
@@ -1,15 +1,149 @@
-import { useContext, Suspense, useState, createContext, lazy } from "react";
+import { useEffect, Suspense, useState, createContext, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { fetchAPI, fetchUser, validateToken } from "./api";
+
+import { readStorage, deleteStorage, writeStorage } from "./localstorage";
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const Index = lazy(() => import("./pages/index"));
 const Game = lazy(() => import("./pages/game"));
 const Login = lazy(() => import("./pages/Login"));
 const Register = lazy(() => import("./pages/Register"));
-//const UserContext = createContext();
 
-function App() {  
-  return(
+interface userContextInterface {
+  authenticated?: boolean;
+  token?: string | null;
+  userData?: Object | null;
+  loginUser?: any
+}
+
+const UserContext = createContext<userContextInterface | null>(null);
+
+const MySwal = withReactContent(Swal);
+
+function App() {
+  const [state, setState] = useState<userContextInterface | null>({
+    authenticated: false,
+  });
+
+  useEffect(() => {
+    let token = readStorage("auth");
+
+    if (!token) {
+      deleteStorage("auth");
+      let updatedState = {
+        ...state,
+        authenticated: false,
+        userData: null,
+        isLoading: false,
+      };
+      setState(updatedState);
+      return;
+    } else {
+      validateToken(token).then((isValid) => {
+        if (isValid) {
+          fetchUser(String(token)).then((userData) => {
+            let updatedState = {
+              userData: userData.data,
+              token,
+              authenticated: true,
+              isLoading: false,
+            };
+
+            setState((prevState) => {
+              return { ...prevState, ...updatedState };
+            });
+          });
+        } else {
+          deleteStorage("auth");
+          let updatedState = {
+            ...state,
+            authenticated: false,
+            userData: null,
+            isLoading: false,
+          };
+          setState(updatedState);
+          return;
+        }
+      });
+    }
+  }, []);
+
+  const loginUser = async (userData: object) => {
+    return new Promise((resolve, reject) => {
+      fetchAPI("/login", userData, {}, "POST")
+        .then((responseData) => {
+          if (responseData.type === "login-success") {
+            fetchUser(responseData.data[0]).then((userDataFromServer) => {
+              let updatedState = {
+                userData: userDataFromServer.data,
+                token: responseData.data[0],
+                authenticated: true,
+              };
+
+              setState((prevState) => {
+                return { ...prevState, ...updatedState };
+              });
+
+              writeStorage("auth", updatedState.token);
+              writeStorage("refresh", responseData.data[1]);
+
+              resolve({});
+            });
+          } else {
+            reject(responseData);
+          }
+        })
+        .catch((error) => {
+          console.log(`Internal server error: ${error}`);
+
+          MySwal.fire(
+            'Oops...',
+            'There was problem with the server. Please try again later!',
+            'error'
+          );
+
+          reject(error);
+        });
+    });
+  }
+
+  const registerUser = async (userData: object) => {
+    return new Promise((resolve, reject) => {
+      fetchAPI("/register", userData, {}, "POST")
+        .then((responseData) => {
+          if (responseData.type === "register-success") {
+            resolve(responseData);
+          } else {
+            reject(responseData);
+          }
+        })
+        .catch((error) => {
+          console.log(`Internal server error: ${error}`);
+
+          MySwal.fire(
+            "Oops...",
+            "There was problem with the server. Please try again later!",
+            "error"
+          );
+
+          reject(error);
+        });
+    });
+  };
+
+  return (
     <Router>
+      <UserContext.Provider
+        value={{
+          authenticated: state?.authenticated,
+          token: state?.token,
+          userData: state?.userData,
+          loginUser
+        }}
+      >
         <Suspense>
           <Routes>
             <Route path="/" element={<Index />} />
@@ -24,8 +158,9 @@ function App() {
             <Route path="/register" element={<Register />} />
           </Routes>
         </Suspense>
+      </UserContext.Provider>
     </Router>
   );
 }
 
-export default App;
+export { App, UserContext };
--- client/game-client/src/pages/Login.tsx
+++ client/game-client/src/pages/Login.tsx
@@ -1,5 +1,60 @@
+import { useCallback, useContext, useState } from "react";
+import { UserContext } from "../App";
+
+
 function Login() {
-  return <div>Login</div>;
+  const userContext = useContext(UserContext);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [successMsg, setsuccessMsg] = useState("");
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      let data = {
+        username: e.target.username.value,
+        password: e.target.pass.value,
+      };
+
+      userContext!.loginUser(data).then(
+        () => {
+          setErrMsg("");
+          setsuccessMsg("Successfully logged in. Redirecting...");
+          //window.location.href = "/account";
+          return;
+        },
+        (err: Array) => {
+          setErrMsg(err.fields.join(", "));
+        }
+      );
+    },
+    [userContext]
+  );
+
+  return (
+    <>
+      <h1>Login</h1>
+      <br />
+      <form method="POST">
+        <label>Username:</label>
+        <br />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Username"
+        />
+        <br />
+        <label>Password:</label>
+        <br />
+        <input type="password" id="pass" name="pass" placeholder="*******" />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
 }
 
 export default Login;
